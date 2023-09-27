de9fd3403c9306a54f9a9e279e34dd4dead3a7b5
NewDoings <87979577+NewDoings@users.noreply.github.com>
2023-09-27 14:36:07 +0300

    Add files via upload

---
+++ twoSumboody/twoSumboody/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoSumboody
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nume = new int[]{ 3,2,1,3};
+            int target = 6;
+            int[] sum = TowSum(nume, target);
+            Console.WriteLine(sum[0].ToString() + sum[1].ToString());
+            Console.ReadKey();
+        }
+        public static int[] TowSum(int[] nume, int target)
+        {
+            int[] sum = new int[2];
+            for (int i = 0; i < nume.Length; i++)
+            {
+                for (int j = i + 1; j < nume.Length; j++)
+                {
+                    if (nume[i] + nume[j] == target)
+                    {
+                        //sum[0] = Array.IndexOf(nume,nume[i]);
+                        //sum[1] = Array.IndexOf(nume, nume[i+1]);
+                        sum[0] = i;
+                        sum[1] = j;
+                        break;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ twoSumboody/twoSumboody/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("twoSumboody")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("twoSumboody")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a6c10ffc-e2a7-4de4-b247-6ddf862c13f8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
